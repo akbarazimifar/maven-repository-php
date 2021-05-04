ebf054976059bbc21ed2b8df5d110d87112e6932
Hossein Zare <zarehossein.hz1997@gmail.com>
2021-05-04 16:45:23 +0430

    Add whitespace to anonymous function.

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -2,7 +2,7 @@
 
 if (! function_exists("join_string")) {
     function join_string(...$args) {
-        $args = array_filter($args, function($x) {
+        $args = array_filter($args, function ($x) {
             return ! empty(trim($x));
         });
 
